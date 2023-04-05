d74c2c7a068a1d50cc9e587d4c216d5305d466bb
Spencer <spencerhommel@gmail.com>
2023-04-05 15:27:30 -0400

    lose game loop and ascii hangman function

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,25 @@ use random_word::gen;
 use std::io;
 
 fn main() {
-    display_intro();
+    print_intro();
     
     // choose random word for user to guess
     let target_word = gen();
 
     println!("random word is: {}", target_word);
 
+    // number of wrong guesses user has, 6 and they lose!
+    let mut wrong_guesses: u8 = 0;
+
     loop {
+        print_hangman(wrong_guesses);
+        
+        // check if user lost
+        if wrong_guesses >= 6 {
+            println!("You lose, the word was {}", target_word);
+            break;
+        }
+
         println!("Enter your guesss: ");
 
         let mut guess = String::new();
@@ -38,14 +49,36 @@ fn main() {
             continue;
         }
 
-        println!("your guess was {}", guess);
+        if !target_word.contains(guess) {
+            wrong_guesses+=1;
+            println!("Incorrect!");
+        }
+
+        else {
+            println!("Correct!");
+        }
+
     }
 
 }
 
-fn display_intro(){
+fn print_intro(){
     println!("Welcome to Hangy, a hangman game written in rust");
     println!("A random word will be chosen... and you need to guess it!");
     println!("To quit at any time, press ~");
 }
 
+fn print_hangman(wrong_guesses: u8) {
+    // prints the proper hangman ascii depending on number of wrong guesses
+
+    let hangman_str: &str = match wrong_guesses {
+        0 => "\n  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
+        1 => "\n  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
+        2 => "\n  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========",
+        3 => "\n  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========",
+        4 => "\n  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n=========",
+        5 => "\n  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n=========",
+        _ => "\n  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n=========",
+    };
+    println!("{}", hangman_str);
+}
