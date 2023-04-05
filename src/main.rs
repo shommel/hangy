9948923976757172578e719d1db563fb3d0be7f0
Spencer <spencerhommel@gmail.com>
2023-04-05 16:28:14 -0400

    added support and handling of already guessed letters

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,55 @@
 use random_word::gen;
 use std::io;
 
+fn print_intro(){
+    println!("Welcome to Hangy, a hangman game written in rust");
+    println!("A random word will be chosen... and you need to guess it!");
+    println!("To quit at any time, press ~");
+}
+
+fn print_hangman(wrong_guesses: u8) {
+    // prints the proper hangman ascii depending on number of wrong guesses
+
+    let hangman_str: &str = match wrong_guesses {
+        0 => "\n  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
+        1 => "\n  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
+        2 => "\n  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========",
+        3 => "\n  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========",
+        4 => "\n  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n=========",
+        5 => "\n  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n=========",
+        _ => "\n  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n=========",
+    };
+    println!("{}", hangman_str);
+}
+
 fn main() {
     print_intro();
     
     // choose random word for user to guess
     let target_word = gen();
 
-    println!("random word is: {}", target_word);
-
     // number of wrong guesses user has, 6 and they lose!
     let mut wrong_guesses: u8 = 0;
 
+    // collection of already guessed letters
+    let mut gusssed_letters = Vec::new();
+
+    println!("random word is: {}", target_word);
+
     loop {
         print_hangman(wrong_guesses);
-        
+
         // check if user lost
         if wrong_guesses >= 6 {
             println!("You lose, the word was {}", target_word);
             break;
         }
+
+        // print sorted collection of user guessed letters
+        println!(
+            "Guessed letters: {}",
+            gusssed_letters.iter().cloned().collect::<String>()
+        );
 
         println!("Enter your guesss: ");
 
@@ -49,6 +79,15 @@ fn main() {
             continue;
         }
 
+        // check for already guessed letters
+        else if gusssed_letters.contains(&guess) {
+            println!("Error: Already guessed {}", guess);
+            continue;
+        }
+
+        gusssed_letters.push(guess);
+        gusssed_letters.sort();
+
         if !target_word.contains(guess) {
             wrong_guesses+=1;
             println!("Incorrect!");
@@ -60,25 +99,4 @@ fn main() {
 
     }
 
-}
-
-fn print_intro(){
-    println!("Welcome to Hangy, a hangman game written in rust");
-    println!("A random word will be chosen... and you need to guess it!");
-    println!("To quit at any time, press ~");
-}
-
-fn print_hangman(wrong_guesses: u8) {
-    // prints the proper hangman ascii depending on number of wrong guesses
-
-    let hangman_str: &str = match wrong_guesses {
-        0 => "\n  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
-        1 => "\n  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
-        2 => "\n  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========",
-        3 => "\n  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========",
-        4 => "\n  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n=========",
-        5 => "\n  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n=========",
-        _ => "\n  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n=========",
-    };
-    println!("{}", hangman_str);
 }
